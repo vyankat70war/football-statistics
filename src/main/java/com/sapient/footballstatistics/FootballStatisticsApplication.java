e013b5e48b627f3c9aec6688e9c2268dc2b5d68a
Vyankat.Sattarwar <Vyankat.Sattarwar@HIRING.PUBLICISSAPIENT.COM>
2021-01-17 12:51:30 +0530

    initial commit

---
+++ src/main/java/com/sapient/footballstatistics/FootballStatisticsApplication.java
@@ -0,0 +1,13 @@
+package com.sapient.footballstatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballStatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballStatisticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sapient/footballstatistics/FootballStatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapient.footballstatistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballStatisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
